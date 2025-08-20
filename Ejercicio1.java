da4449f9fa8d3d926e7034749497a446aab120e7
Progra2-2690-24-4072 <ltaracenam1@miumg.edu.gt>
2025-08-19 23:06:14 -0600

    Update Ejercicio1.java

--- Ejercicio1.java
+++ Ejercicio1.java
@@ -6,12 +6,12 @@ public class Ejercicio1 {
 
     public static void main(String[] args) {
         ArrayList<String> companeros = new ArrayList<>();
-        companeros.add("Ana");
-        companeros.add("Luis");
-        companeros.add("Carlos");
+        companeros.add("Juan");
+        companeros.add("Paco");
+        companeros.add("jos");
         companeros.add("Marta");
-        companeros.add("Sofia");
-        companeros.add("Javier");
+        companeros.add("amparo");
+        companeros.add("Marcela");
 
         for (String nombre : companeros) {
             System.out.println(nombre);
